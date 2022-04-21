7234944273072fb89886b243bc4831e21f20907a
Satyen Sabnis <97633870+satyensab@users.noreply.github.com>
2022-04-21 17:40:12 -0400

    Update fake_news.class.py

--- fake_news.class.py
+++ fake_news.class.py
@@ -18,25 +18,20 @@ class fake_News:
     labels = df.label
     print("\nFirst 5 labels: " + str(labels.head()))
 
-    #DataFlair - Split the dataset
+
     x_train,x_test,y_train,y_test=train_test_split(df['text'], labels, test_size=0.2, random_state=7)
 
-    #Initialize a TfidfVectorizer
     tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)
-    #Fit and transform train set, transform test set
     tfidf_train=tfidf_vectorizer.fit_transform(x_train) 
     tfidf_test=tfidf_vectorizer.transform(x_test)
 
-    #Initialize a PassiveAggressiveClassifier
     pac=PassiveAggressiveClassifier(max_iter=50)
     pac.fit(tfidf_train,y_train)
 
-    #Predict on the test set and calculate accuracy
     y_pred=pac.predict(tfidf_test)
     score=accuracy_score(y_test,y_pred)
     print(f'\nAccuracy: {round(score*100,2)}%')
 
-    #DataFlair - Build confusion matrix
     conf_mat = confusion_matrix(y_test,y_pred, labels=['FAKE','REAL'])
     print("\nConfusion Matrix: " + "\n" +str(conf_mat))
 
